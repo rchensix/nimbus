e3274c82362a6bcf784ff08cc403162e566c6c33
Ruiqi Chen <rchensix@gmail.com>
2025-02-09 10:28:13 -0800

    recompile

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,7 @@
+/* To compile:
+./emsdk/upstream/emscripten/emcc hello.cpp -o hello.js -s WASM=1 -s NO_EXIT_RUNTIME=1 --bind -s MODULARIZE=1 -s EXPORT_ES6=1
+*/
+
 #include <string>
 
 #include <emscripten/bind.h>
